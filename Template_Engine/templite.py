03cb4b1cf9da48bd19a200b4a42e3e164e812f80
sunshead <sunshead@gmail.com>
2016-06-24 23:42:56 -0400

    Add CodeBuilder Class

--- Template_Engine/templite.py
+++ Template_Engine/templite.py
@@ -13,3 +13,46 @@ text = templite.rendr({
 	'name': "Ned",
 	'topics': ['Python', 'Geometry', 'Juggling'],
 	})
+
+#A CodeBuilder object keeps a list of strings that will together be the final Python code.
+class CodeBuilder(object):
+	"""Build source code conviently."""
+
+	def __init__(self, indent=0):
+		self.code = []
+		self.indent_level = indent
+
+	def add_line(self, line):
+		#Add a line of source to the code
+		self.code.extend([" " * self.indent_level, line, "\n"])
+
+	INDENT_STEP = 4
+
+	def indent(self):
+		#Increase the current indent for following lines.
+		self.indent_level += self.INDENT_STEP
+
+	def dedent(self):
+		#Decrease the current indent for following lines.
+		self.indent_level -= self.INDENT_STEP
+
+	def add_section(self):
+		#Add a section -- a sub-CodeBuilder.
+		section = CodeBuilder(self.indent_level)
+		self.code.append(section)
+		return section
+
+	def __str__(self):
+		return "".join(str(c) for c in self.code)
+
+	def get_globals(self):
+		#Execute the code, and return a dict of globals it defines.
+
+		#Check that the caller has finished all the blocks they started
+		assert self.indent_level == 0
+		#Get the Python source as a single string
+		python_source = str(self)
+		#Execute the source, defining globals, and return them
+		global_namespace = {}
+		exec(python_source, global_namespace)
+		return global_namespace
